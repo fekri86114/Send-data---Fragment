f0036061a340ff685a72c386476d19564c47e45c
fekri1386 <fekri.mohammadreza86@gmail.com>
2022-08-27 18:53:06 +0430

    worked with AlphaAnimation

--- app/src/main/java/com/example/androidtraining/MainActivity.kt
+++ app/src/main/java/com/example/androidtraining/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.androidtraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import com.example.androidtraining.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,14 +15,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnOpenFirstFragment.setOnClickListener {
-
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.add(R.id.frame_main_container, FirstFragment())
-            transaction.addToBackStack(null)
-            transaction.commit()
+        binding.btnAnim.setOnClickListener {
+            alphaAnimation()
 
         }
+
+    }
+
+    private fun alphaAnimation() {
+
+        val anim = AlphaAnimation(1f, 0f)
+
+        // for all animations --> from here
+        anim.duration = 1000
+        anim.fillAfter = true
+        anim.repeatCount = 5
+        anim.repeatMode = Animation.REVERSE
+        anim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+                Log.v("testAnimation", "Anim started!")
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                Log.v("testAnimation", "Anim ended!")
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+                Log.v("testAnimation", "Anim repeated!")
+            }
+        })
+        // to here
+
+        binding.imgAnim.startAnimation(anim)
 
     }
 }
